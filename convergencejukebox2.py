38677d89ff59c11148a65ae571e0c43c7e0b80be
bradfortner <bradfortner@gmail.com>
2017-07-09 14:34:03 -0400

    Ellipse to canvas using KV language from startup screen. Centred and sized.

--- convergencejukebox2.py
+++ convergencejukebox2.py
@@ -196,9 +196,11 @@ Builder.load_string('''
 <PongBall>:
     size: 1, 1
     canvas:
+        Color:
+            rgb: 1, 1, 1
         Ellipse:
-            pos: self.pos
-            size: self.size
+            pos: 600,290
+            size: 70,70       
 <FloatLayout>:
     id: selection
     canvas.before:
@@ -206,8 +208,7 @@ Builder.load_string('''
             source: 'jukebox.png'
             pos: self.pos
             size: self.size
-    PongBall:
-        
+      
     Button:
         id: song_playing_name
         text: ' '
@@ -241,6 +242,8 @@ Builder.load_string('''
         width: 1280
         height: 720
         background_color: 0,0,0,0
+    PongBall:
+        id: moving_ball
     Label:
         id: jukebox_name
         text: "Convergence Music System 2.0"
@@ -1638,8 +1641,9 @@ class JukeboxScreen(FloatLayout):
     def message_at_start(self, *args):
         self.ids.my_fullscreen_blackout.background_color = (0, 0, 0, 1)
         self.ids.my_fullscreen_blackout.text = "This Is My Startup Screen"
-        self.ids.my_fullscreen_blackout.color = (1, 1, 1, 1)
+        #self.ids.my_fullscreen_blackout.color = (1, 1, 1, 1)
         self.ids.my_fullscreen_blackout.font_size = 25
+        self.ids.moving_ball.size = (0,0)
 
     class PongBall(Widget):
 
